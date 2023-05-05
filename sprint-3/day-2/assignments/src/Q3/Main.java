02bed29e74588e19eaa68f6a1f21b343dc4b62dc
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-06 00:12:56 +0530

    que 3

---
+++ sprint-3/day-2/assignments/src/Q3/Main.java
@@ -0,0 +1,13 @@
+package Q3;
+
+public class Main {
+	public static void main(String[] args) {
+		ThreadA ta=new ThreadA();
+		ThreadB tb=new ThreadB(ta);
+		
+		ta.start();
+		tb.start();
+		
+	}
+
+}
